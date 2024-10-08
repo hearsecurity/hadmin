1ab50a97e3cef13098655072f1e3722ddec67242
HearSecurity <hearsectech@gmail.com>
2024-10-07 20:14:54 -0400

    Update hadmin.py

--- hadmin.py
+++ hadmin.py
@@ -3,25 +3,35 @@
 import requests
 import sys
 import concurrent.futures
-from colorama import Fore, Back, Style
+from colorama import Fore
+from time import sleep 
+from pathlib import Path
 
 paths = []
+
+def banner(): 
+
+  print """
+  _               _           _       
+ | |__   __ _  __| |_ __ ___ (_)_ __  
+ | '_ \ / _` |/ _` | '_ ` _ \| | '_ \ 
+ | | | | (_| | (_| | | | | | | | | | |
+ |_| |_|\__,_|\__,_|_| |_| |_|_|_| |_|                                   
+ """
 
 def main_function(path):
     
     path = path.strip('\n')
     url = sys.argv[1]
 
-    print "Trying: "+url+path   
+    print "Trying: "+url+"/"+path   
     response = requests.get(url+"/"+path)
     code = response.status_code
 
-    if code == 200:
-        print("\n\n\n\n\n\n")
-        print("[FOUND]: "+url+path)
-        print("\n\n\n\n\n\n")
+    if code == 200 and "<input type=" in response.content:
+        print(Fore.GREEN+"[FOUND]: "+url+path+Fore.WHITE)
         f = open("admin.txt", "a")
-        f.write(url+path+"\n")
+        f.write(url+"/"+path+"\n")
         f.close() 
     
 def load_domains(domains):
@@ -36,8 +46,20 @@ def load_domains(domains):
 
 load_domains("paths.txt")
 
-print("\n")
-with concurrent.futures.ThreadPoolExecutor(max_workers = 25) as executor:
+if len(sys.argv) < 2: 
+  banner()
+  print "  Usage: python2 hadmin.py <domain>\n"
+else:
+  banner()
+  print " [*] Wait a few seconds... \n"
+  sleep(2)
+  print("\n")
+  with concurrent.futures.ThreadPoolExecutor(max_workers = 25) as executor:
     executor.map(main_function, paths)
-
+  
+  my_file = Path("admin.txt")
+  if my_file.is_file():
+    print "[+] Admin page found check admin.txt..."
+  else:
+    print "[-] Admin page not found..."
 
